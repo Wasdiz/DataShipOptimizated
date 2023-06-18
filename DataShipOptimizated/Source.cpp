23ed5635e0545c6b4a200552ea95f8a33770d9ab
Zhrat <Zhrat@DESKTOP-OPL4EDL>
2023-06-18 21:25:49 +0700

    new sort

--- DataShipOptimizated/Source.cpp
+++ DataShipOptimizated/Source.cpp
@@ -4,6 +4,9 @@
 #include <string>
 #include <conio.h>
 #include <fstream>
+#include <algorithm>
+#include <sstream>
+#include "Source.h"
 
 
 using namespace std;
@@ -16,6 +19,10 @@ public:
 	int refuel;
 	string type;
 	string name;
+	string str;
+	int numberShip = 0;
+
+	fstream fs;
 
 	SpaceShip(string n, string t, int s, int f, int r) {
 
@@ -26,29 +33,152 @@ public:
 		refuel = r;
 
 	}
-	friend ostream& operator<<(ostream& os, const SpaceShip& ss) {
-		os << ss.name << " | " << ss.type << " type "<<" | " << ss.speed << " speed " << " | " << ss.fuel << " fuel "<<" | " << ss.refuel << " not refuel" << endl;
-		return os;
+	
+
+	void finShip(string name, string type, int speed, int fuel, double refuel)
+	{
+		fs.open("Data.txt", fstream::in | fstream::out | fstream::app);
+		if (!fs.is_open())
+		{
+			cout << "File not open";
+		}
+		else
+		{
+			fs << name << " " << type << " " << speed << " " << fuel << " " << refuel << " " << "\n";
+
+		}
+		fs.close();
 
 	}
+
+	void foutShip()
+	{
+		fs.open("Data.txt", fstream::in | fstream::out | fstream::app);
+		if (!fs.is_open())
+		{
+			cout << "File not open";
+		}
+		else
+		{
+			str = " ";
+			numberShip = 0;
+
+			while (!fs.eof())
+			{
+				numberShip++;
+				cin.ignore();
+				getline(fs, str);
+				cout << numberShip << " " << str;
+			}
+
+		}
+		fs.close();
+	}
+
+	void delShip(int delstr)
+	{
+		
+
+		fstream file("Data.txt", ios::in | ios::out);
+
+		if (file.is_open()) {
+			ofstream temp("temp.txt");
+
+			string line;
+			int delstrtest = 0;
+			while (getline(file, line))
+			{
+				delstrtest++;
+				if (delstr != delstrtest) {
+					temp << line << endl;
+				}
+			}
+
+			file.close();
+			temp.close();
+
+			remove("Data.txt");
+			rename("temp.txt", "Data.txt");
+
+			cout << "Line removed from file." << endl;
+		}
+		else
+		{
+			cout << "Unable to open file." << endl;
+		}
+	}
+
+	void foutSortShip()
+	{
+		fs.open("sorted_data.txt", fstream::in | fstream::out | fstream::app);
+		if (!fs.is_open())
+		{
+			cout << "Ошибка открытия файла!";
+		}
+		else
+		{
+			str = " ";
+			numberShip = 0;
+
+			while (!fs.eof())
+			{
+				numberShip++;
+				cin.ignore();
+				getline(fs, str);
+				cout << numberShip << " " << str;
+			}
+
+		}
+		fs.close();
+	}
+
+	void outsortShip(vector<string> lines)
+	{
+		ofstream outputFile("sorted_data.txt");
+		for (const auto& line : lines)
+		{
+			outputFile << line << endl;
+		}
+		outputFile.close();
+	}
+
 };
+
+std::vector<std::string> split(const std::string& str, char delimiter) {
+	std::vector<std::string> tokens;
+	std::string token;
+	std::istringstream tokenStream(str);
+	while (getline(tokenStream, token, delimiter)) {
+		tokens.push_back(token);
+	}
+	return tokens;
+}
+
+bool compareColumns(const std::string& str1, const std::string& str2, int column) {
+	std::vector<std::string> values1 = split(str1, ' ');
+	std::vector<std::string> values2 = split(str2, ' ');
+	if (values1.size() <= column || values2.size() <= column) {
+		return false; // Некорректные данные
+	}
+	return values1[column] < values2[column];
+}
+
 vector <SpaceShip> UserShips;
-SpaceShip* Ship = new SpaceShip("","",0,0,0);
+SpaceShip* Ship = new SpaceShip("", "", 0, 0, 0);
 int userChoose;
 void DisplayMenu() {
-	
-		cout << "------------------------" << endl;
-		cout << "1: create ship" << endl;
-		cout << "2: show all ships" << endl;
-		cout << "3: edit ship" << endl;
-		cout << "4: delete ship" << endl;
-		cout << "5: save ship" << endl;
-		cout << "6: exit" << endl;
-		cout << "choose action -> ";
-		cin >> userChoose;
-		cout << "------------------------" << endl;
-		cout << endl;
-	}
+
+	cout << "------------------------" << endl;
+	cout << "1: create ship" << endl;
+	cout << "2: show all ships" << endl;
+	cout << "3: sort Ship" << endl;
+	cout << "4: delete ship" << endl;
+	cout << "5: exit" << endl;
+	cout << "choose action -> ";
+	cin >> userChoose;
+	cout << "------------------------" << endl;
+	cout << endl;
+}
 
 
 
@@ -79,20 +209,20 @@ void CreateShip() {
 	cin >> userShipFuel;
 	Ship->fuel = userShipFuel;
 	cout << endl;
-	int userShipRefuel = userShipSpeed / (userShipFuel * 2);
-	
+
+	cout << "Enter additional fuel tanks ";
+	int userShipRefuel = 0;
+	cin >> userShipRefuel;
 	Ship->refuel = userShipRefuel;
 
 	UserShips.push_back(SpaceShip(Ship->name, Ship->type, Ship->speed, Ship->fuel, Ship->refuel));
+
+	Ship->finShip(Ship->name, Ship->type, Ship->speed, Ship->fuel, Ship->refuel);
 }
 
 void ShowAllShip() {
 
-	for (int i = 0; i < UserShips.size();i++) {
-
-		cout << UserShips[i];
-
-	}
+	Ship->foutShip();
 
 	cout << endl;
 
@@ -101,206 +231,22 @@ int userChooseShipToEdit;
 int userShipSpeedEdit;
 int PersonalSelectCreateShipChoose;
 int userChoiseEdit;
-void PersonalSelectCreateShip() {
-	do
-	{
-	cout << UserShips[userChooseShipToEdit] << endl;
-	cout << "1: Name" << endl;
-	cout << "2: Type" << endl;
-	cout << "3: Speed" << endl;
-	cout << "4: Fuel" << endl;
-	cout << "5: Exit" << endl;
-	cout << "Set action: ";
-	cin >> PersonalSelectCreateShipChoose;
-	system("cls");
-	switch (PersonalSelectCreateShipChoose)
-	{
-	case 1: {
-		cout << UserShips[userChooseShipToEdit] << endl;
-		cout << "Name -> ";
-		string userShipNameEdit;
-		cin >> userShipNameEdit;
-		Ship->name = userShipNameEdit;
-		UserShips.at(userChooseShipToEdit) = SpaceShip(Ship->name, Ship->type, Ship->speed, Ship->fuel, Ship->refuel);
-		cout << endl;
-		system("cls");
-		break;
-	}
-	case 2: {
-		cout << UserShips[userChooseShipToEdit] << endl;
-		cout << "Type -> ";
-		string userShipTypeEdit;
-		cin >> userShipTypeEdit;
-		Ship->type = userShipTypeEdit;
-		UserShips.at(userChooseShipToEdit) = SpaceShip(Ship->name, Ship->type, Ship->speed, Ship->fuel, Ship->refuel);
-		cout << endl;
-		system("cls");
-		break;
-	}
-	case 3: {
-		cout << UserShips[userChooseShipToEdit] << endl;
-		cout << "Speed -> ";
-		cin >> userShipSpeedEdit;
-		Ship->speed = userShipSpeedEdit;
-		UserShips.at(userChooseShipToEdit) = SpaceShip(Ship->name, Ship->type, Ship->speed, Ship->fuel, Ship->refuel);
-		cout << endl;
-		system("cls");
-		break;
-	}
-	case 4: {
-		cout << UserShips[userChooseShipToEdit] << endl;
-		cout << "Fuel -> ";
-		int userShipFuelEdit;
-		int userShipRefuelEdit;
-		cin >> userShipFuelEdit;
-		Ship->fuel = userShipFuelEdit;
-		cout << endl;
-		userShipRefuelEdit = userShipSpeedEdit / (userShipFuelEdit * 2);
-		Ship->refuel = userShipRefuelEdit;
-		UserShips.at(userChooseShipToEdit) = SpaceShip(Ship->name, Ship->type, Ship->speed, Ship->fuel, Ship->refuel);
-		system("cls");
-		break;
-	}
-	case 5: {
-		/*cout << "Edit saved: ";
-		cout << endl;*/
-		break;
-		
-	}
-	default: {
-		cout << "Wrong action" << endl;
-		system("cls");
-		break;
-	}
-	}
-	} while (PersonalSelectCreateShipChoose !=5);
 
 
-}
-
-void EditShip() {
-
-	ShowAllShip();
-
-	cout << "Choose ship: ";
-	cin >> userChooseShipToEdit;
-	userChooseShipToEdit--;
-
-	system("cls");
-
-	PersonalSelectCreateShip();
-
-	UserShips.at(userChooseShipToEdit) = SpaceShip(Ship->name, Ship->type, Ship->speed, Ship->fuel, Ship->refuel);
-}
-
-void remove(){
-
-	}
-
-void DeleteShip() {
-
-	ShowAllShip();
-
-	cout << "Choose ship: ";
-	cin >> userChooseShipToEdit;
-	userChooseShipToEdit--;
-
-	system("cls");
-
-	UserShips.erase(UserShips.begin()+ userChooseShipToEdit);
-}
-
-void SaveShip() {
-
-	ofstream Data("C:\\Users\\Zhrat\\source\\repos\\DataShipOptimizated\\DataShipOptimizated\\Data.txt");
-
-	if (Data.is_open()) {
-
-		for (auto s : UserShips) {
-
-			Data << s.name << " | " << s.type << " | " << s.speed << " | " << s.fuel << " | " << s.refuel <<endl;
 
 
-		}
-		cout << "Ships saved"<< endl; 
-		Data.close();
-
-	}
-	else
-	{
-		cout << "File not open";
-	}
-
-}
-
-void LoadShip() {
-	
-	ifstream Data("C:\\Users\\Zhrat\\source\\repos\\DataShipOptimizated\\DataShipOptimizated\\Data.txt");
-
-	if (Data.is_open()) {
-
-		string line;
-
-		while (getline(Data, line))
-		{
-			string type;
-			string name;
-			int speed;
-			int fuel;
-			int refuel;
-			
-			size_t pos = 0;
-			string token;
-			int i = 0;
-
-			while ((pos = line.find(" | ")) !=  string::npos )
-			{
-				token = line.substr(0, pos);
-				switch (i)
-				{
-				case 1: {
-					name = token;
-					break;
-				}
-				case 2: {
-					type = token;
-					break;
-				}
-				case 3: {
-					speed = stoi(token);
-					break;
-				}
-				case 4: {
-					fuel = stoi(token);
-					break;
-				}
-				case 5: {
-					refuel = stoi(token);
-					break;
-				}
-				}
-				line.erase(0, pos + 1);
-				i++;
-			}
-			SpaceShip s(name, type, speed, fuel, refuel);
-			UserShips.push_back(s);
-		}
-		cout << "Ships loaded" << endl;
-		Data.close();
-	}
-	else
-	{
-		cout << "File not open";
-	}
-
-}
+int delstr;
 
 
 int main() {
+	ifstream inputFile("Data.txt");
+	vector<std::string> lines;
+	string line;
+
+	int columnToSort = 0;
 	do
 	{
 		system("cls");
-		LoadShip();
 		DisplayMenu();
 		switch (userChoose)
 		{
@@ -319,22 +265,36 @@ int main() {
 		}
 		case 3: {
 			system("cls");
-			EditShip();
+			cout << "Enter number of colum to sort: ";
+			cin >> columnToSort;
+			columnToSort = columnToSort - 1;
+
+			while (getline(inputFile, line))
+			{
+				lines.push_back(line);
+			}
+			inputFile.close();
+
+
+			sort(lines.begin(), lines.end(), [columnToSort](const std::string& str1, const std::string& str2)
+				{
+					return compareColumns(str1, str2, columnToSort);
+				});
+
+			Ship->outsortShip(lines);
+
+			Ship->foutSortShip();
+
 			break;
 		}
 		case 4: {
 			system("cls");
-			DeleteShip();
+			cin >> delstr;
+			Ship->delShip(delstr);
 			break;
 		}
+		
 		case 5: {
-			system("cls");
-			SaveShip();
-			cout << "Press any key to go menu";
-			_getch();
-			break;
-		}
-		case 6: {
 			system("cls");
 			cout << "Goodbye";
 			break;
@@ -347,5 +307,8 @@ int main() {
 
 	} while (userChoose != 6);
 
-	
+
 }
+
+
+
